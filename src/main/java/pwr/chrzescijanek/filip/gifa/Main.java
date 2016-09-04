c9691296806de9cb521dedeeb5f19194f65c8696
Filip Chrześcijanek <filip702@gmail.com>
2016-09-04 23:39:12 +0200

    Basic affine transformations done

--- src/main/java/pwr/chrzescijanek/filip/gifa/Main.java
+++ src/main/java/pwr/chrzescijanek/filip/gifa/Main.java
@@ -5,10 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 public class Main extends Application {
 
@@ -25,25 +24,67 @@ public class Main extends Application {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample.fxml"));
 		Parent root = loader.load();
 		Controller controller = loader.getController();
-		printSample(controller);
+		controller.setLabel("Controller test");
+		doSample();
 		primaryStage.setTitle("Hello World");
 		primaryStage.setScene(new Scene(root, 640, 480));
 		primaryStage.show();
 	}
 
-	private void printSample( final Controller controller ) {
-		Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-		Mat m2 = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-		m.copyTo(m2);
-		Mat mr1 = m.row(1);
-		mr1.setTo(new Scalar(1));
-		Mat mc5 = m.col(5);
-		mc5.setTo(new Scalar(5));
+	private void performTransformations( Mat[] images, MatOfPoint2f[] points ) {
+		assert ( images.length == points.length );
+		for ( Mat m : images )
+			Imgproc.cvtColor(m, m, Imgproc.COLOR_BGR2BGRA);
+		Mat result = new Mat(images[0].size(), images[0].type());
+		for ( int i = 1; i < images.length; i++ ) {
+			Mat warpMat = Imgproc.getAffineTransform(points[i], points[0]);
+			Imgproc.warpAffine(images[i], images[i], warpMat, images[0].size(),
+					Imgproc.INTER_NEAREST, Core.BORDER_CONSTANT, new Scalar(0, 0, 0, 0));
+		}
+		for ( int i = 0; i < images[0].rows(); i++ ) {
+			for ( int j = 0; j < images[0].cols(); j++ ) {
+				double[] data = new double[4];
+				for ( Mat m : images ) {
+					double[] imgData = m.get(i, j);
+					data[0] += imgData[0] * imgData[3] / ( images.length * 255 );
+					data[1] += imgData[1] * imgData[3] / ( images.length * 255 );
+					data[2] += imgData[2] * imgData[3] / ( images.length * 255 );
+				}
+				data[3] = 255;
+				result.put(i, j, data);
+			}
+		}
+		for ( int i = 0; i < result.rows(); i++ ) {
+			for ( int j = 0; j < result.cols(); j++ ) {
+				boolean isTransparent = false;
+				for ( Mat m : images ) {
+					double[] imgData = m.get(i, j);
+					isTransparent = Math.abs(imgData[3] - 0.0) < 0.0000001;
+					if ( isTransparent ) break;
+				}
+				if ( isTransparent ) {
+					for ( Mat m : images ) {
+						double[] data = { 0.0, 0.0, 0.0, 0.0 };
+						m.put(i, j, data);
+					}
+				}
+			}
+		}
+		Imgcodecs.imwrite("src/main/resources/result.png", result);
+		for ( int i = 0; i < images.length; i++ ) {
+			Imgcodecs.imwrite("src/main/resources/image" + i + ".png", images[i]);
+		}
+	}
 
-		String label = String.format("Welcome to OpenCV %s\n\n"
-						+ "OpenCV Mat: %s\n\n"
-						+ "OpenCV Mat data:\n%s",
-				Core.VERSION, m.toString(), m.dump());
-		controller.setLabel(label);
+	private void doSample() {
+		Mat[] images = new Mat[3];
+		images[0] = Imgcodecs.imread("src/main/resources/tri.jpg", Imgcodecs.CV_LOAD_IMAGE_ANYCOLOR);
+		images[1] = Imgcodecs.imread("src/main/resources/tri-aff.jpg", Imgcodecs.CV_LOAD_IMAGE_ANYCOLOR);
+		images[2] = Imgcodecs.imread("src/main/resources/tri-new.jpg", Imgcodecs.CV_LOAD_IMAGE_ANYCOLOR);
+		MatOfPoint2f[] points = new MatOfPoint2f[3];
+		points[0] = new MatOfPoint2f(new Point(58, 10), new Point(13, 97), new Point(103, 97));
+		points[1] = new MatOfPoint2f(new Point(70, 10), new Point(25, 97), new Point(115, 97));
+		points[2] = new MatOfPoint2f(new Point(127, 57), new Point(40, 12), new Point(40, 102));
+		performTransformations(images, points);
 	}
 }
